e6566873034d6f6f2cd1269b2b89a74eb138bd4e
SHRUTI DALVI <shrutimdalvi14@gmail.com>
2023-10-13 00:35:35 +0530

    Adding FAQ

--- src/components/Faq.js
+++ src/components/Faq.js
@@ -1,12 +1,56 @@
 import '../style/faq.css'
-function Faq(){
-    return(
-        <div className="FaqMainContainer">
-            <div className='FaqMainDiv'>
-            <h1>FAQ</h1>
+import React, { useState } from 'react';
+
+ function Faq() {
+    const [openIndex, setOpenIndex] = useState(-1);
+  
+    const toggleAccordion = (index) => {
+      if (openIndex === index) {
+        setOpenIndex(-1);
+      } else {
+        setOpenIndex(index);
+      }
+    };
+ 
+    return (
+
+      <div className="FaqMainContainer">
+        <div className="FaqMainDiv">
+        <h1>FAQs</h1>
+          {data.map((item, i) => (
+            <div className="item" key={i}>
+              <div className="ques" onClick={() => toggleAccordion(i)}>
+              <span className={`icon ${openIndex === i ? 'minus' : 'plus'}`}></span>
+                <h2>{item.question}</h2>
+              </div>
+              <div className={`ans ${openIndex === i ? 'open' : ''}`}>
+                <h3>{item.answer}</h3>
+              </div>
             </div>
-            
+          ))}
         </div>
-    )
-}
+      </div>
+    );
+  }
+
+
+const data=[
+    {
+        question: 'What is a Google Developer Student Club (GDSC)?',
+        answer: ' GDSC is a community of student developers who are passionate about Google technologies. It organize events, workshops and seminars to foster learning.'
+    },
+    {
+         question: 'Who should join Google Developer Student Clubs?',
+         answer: ' College and university students are encouraged to join Google Developer Student Clubs.'
+    },
+    {
+      question: 'Do I need prior coding experience to join GDSC? ',
+      answer: 'No, prior coding experience is not needed. GDSC is open to all, including beginners. '  
+    },
+    {
+        question: 'What kind of events does GDSC organize?',
+        answer: 'GDSC host a variety of events, including coding workshops, hackathons, seminars, and networking events.'
+    }
+]
+
 export default Faq;
