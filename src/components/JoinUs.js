b7172d7c0210f46f493779768f8c76573a700f9c
ananthvgb2003 <ananthvgb.1006@gmail.com>
2023-10-28 00:50:19 +0530

    Logo is now centered

--- src/components/JoinUs.js
+++ src/components/JoinUs.js
@@ -6,7 +6,7 @@ function JoinUs() {
       <div className="w-full flex justify-center h-joinus pt-32">
         <div className="w-full h-joinus flex rounded-tl-joinus-left rounded-tr-joinus-right justify-center flex-col items-center text-center bg-grey backdrop-blur-10 border border-solid border-joinus-grey shadow-joinus pb-80 pt-56">
             <div className="logo">
-            <img src="/joinUsLogo.png" alt="Logo" style={{ width:'90%' }} />
+            <img src="/joinUsLogo.png" alt="Logo" style={{ width: '100%', transform: 'scale(0.9)' }} />
             </div>
         </div>
     </div>
